a59e6c976e1fc7e51c45e37218ddf8941215f4f3
Florian Topf <florian.topf@oeaw.ac.at>
2011-04-28 19:59:10 +0000

    next step to comply xhtml strict in index

--- index.php
+++ index.php
@@ -53,17 +53,17 @@
       <div id="header-top">
         <div id="header-top-inside">
           <div id="logo">
-            <a href="http://europlanet-jra3.oeaw.ac.at" target="_blank"><img src="images/europlanet_logo.gif" width="561" height="51" border="0" alt="EuroPlanet - Research Infrastructure." title="EuroPlanet - Research Infrastructure." /></a>
+            <a href="http://europlanet-jra3.oeaw.ac.at" target="_blank"><img src="images/europlanet_logo.gif" width="561" height="51" alt="EuroPlanet - Research Infrastructure." title="EuroPlanet - Research Infrastructure." /></a>
           </div>
           <div id="partners">
-            <a href="http://europa.eu/index_en.htm" target="_blank"><img src="images/ue_logo.gif" width="81" height="47" border="0" alt="Project supported by the European Union." title="Project supported by the European Union." /></a>
-            <a href="http://cordis.europa.eu/fp7/" target="_blank"><img src="images/7_logo.gif" width="77" height="53" border="0" alt="Seventh Framework Programme." title="Seventh Framework Programme." /></a>
+            <a href="http://europa.eu/index_en.htm" target="_blank"><img src="images/ue_logo.gif" width="81" height="47" alt="Project supported by the European Union." title="Project supported by the European Union." /></a>
+            <a href="http://cordis.europa.eu/fp7/" target="_blank"><img src="images/7_logo.gif" width="77" height="53" alt="Seventh Framework Programme." title="Seventh Framework Programme." /></a>
           </div>
         </div>
       </div>
       <div id="header-img"></div>
     </div>
-	<form action="<?php print $_SERVER["PHP_SELF"]; ?>" name="main_form" id="main_form" enctype="multipart/form-data" method="post">
+	<form action="<?php print $_SERVER["PHP_SELF"]; ?>" id="main_form" enctype="multipart/form-data" method="post">
 
     <?php
       if (!isSet($_POST["page"]))
@@ -76,63 +76,63 @@
     ?>
 
     <div id="top">
-		<div id="top-menu-img"><img src="images/top_menu_img.gif" align="middle" width="527" height="19" border="0" alt="top menu" title="top menu" /></div>
+		<div id="top-menu-img"><img src="images/top_menu_img.gif" align="middle" width="527" height="19" alt="top menu" title="top menu" /></div>
 			<div id="top-menu"><font style="font-family: verdana, arial; font-weight: bold; font-size: 22px;" color="#172983">NA1-MATRIX of ground-based &amp; space-based observatories</font></div>
-		<div id="top-menu-bottom-img"><img src="images/bottom_menu_img.gif" align="middle" width="527" height="19" border="0" alt="top menu" title="top menu" /></div>
+		<div id="top-menu-bottom-img"><img src="images/bottom_menu_img.gif" align="middle" width="527" height="19" alt="top menu" title="top menu" /></div>
 	</div>
 
     <div id="content-wrapper">
-    <CENTER>
-    <TABLE class="main" border="0" cellspacing="0" cellpadding="0" width="100%" height="100%">
-      <TR><!--LEFT MENU-->
-        <TD valign="top"><IMG src="images/blank.gif" width="0" border="0"></TD>
-        <TD valign="top" id="lmcol" width="208">
-          <TABLE class="ltable" cellspacing="0" cellpadding="0" width="100%">
+    <center>
+    <table class="main" border="0" cellspacing="0" cellpadding="0" width="100%" height="100%">
+      <tr><!--LEFT MENU-->
+        <td valign="top"><img src="images/blank.gif" width="0" /></td>
+        <td valign="top" id="lmcol" width="208">
+          <table class="ltable" cellspacing="0" cellpadding="0" width="100%">
             <div id="left">
 		    <div id="left-content">
 		    <div id="left-menu-box">
             <div id="left-menu">
               <ul>
-                <li class="left-level-1-no"><A href="<?php print $_SERVER["PHP_SELF"]; ?>?page=home">Home</A></li>
-                <li class="left-level-1-no"><A href="<?php print $_SERVER["PHP_SELF"]; ?>?page=browse">Browse Matrix</A></li>
+                <li class="left-level-1-no"><a href="<?php print $_SERVER["PHP_SELF"]; ?>?page=home">Home</a></li>
+                <li class="left-level-1-no"><a href="<?php print $_SERVER["PHP_SELF"]; ?>?page=browse">Browse Matrix</a></li>
 
           <?php
           	if ($_SESSION["user_id"] <= 1)
             {
-              print "<div align='center'><li><FONT font-weight='bold'; font-size='10px'; color='black';>Username: </FONT>" .
-                    "<INPUT name='userid' size='10'></li></div>" . LF;
-              print "<div align='center'><li><FONT font-weight='bold'; font-size='10px'; color='black';>Password: </FONT>" .
-                    "<INPUT type='password' name='passwd' size='10'></li></div>" . LF;
-              print "<div align='right'><li><INPUT type='submit' name='login' value='Log in'></li></div>" . LF;
-              print "<div align='center'><li><A href='" . $_SERVER["PHP_SELF"] . "?page=reset'>Forgot password?</A></li></div>" .LF;
+              print "<div align='center'><li><font font-weight='bold'; font-size='10px'; color='black';>Username: </font>" .
+                    "<input name='userid' size='10'></li></div>" . LF;
+              print "<div align='center'><li><font font-weight='bold'; font-size='10px'; color='black';>Password: </font>" .
+                    "<input type='password' name='passwd' size='10'></li></div>" . LF;
+              print "<div align='right'><li><input type='submit' name='login' value='Log in'></li></div>" . LF;
+              print "<div align='center'><li><a href='" . $_SERVER["PHP_SELF"] . "?page=reset'>Forgot password?</a></li></div>" .LF;
 			}
             else
             {
               print "<h1 class='menu-header'>&nbsp;Edit Matrix:</h1>" . LF;
-              print "<li class='left-level-1-no'><A href='" . $_SERVER["PHP_SELF"] . "?page=add&action=add'>Add entries</A></li>" . LF;
-              print "<li class='left-level-1-no'><A href='" . $_SERVER["PHP_SELF"] . "?page=edit'>Edit entries</A></li>" .LF;
-              print "<div align='center'><li><B>" . $_SESSION["user_name"] . " logged in</B></li></div>" . LF;
-              print "<div align='center'><li><A href='" . $_SERVER["PHP_SELF"] . "?page=account'>My account</A></li></div>" . LF;
-              print "<div align='center'><li><INPUT type='submit' class='cancel' name='logout' value='Log out'></li></div>" . LF;
+              print "<li class='left-level-1-no'><a href='" . $_SERVER["PHP_SELF"] . "?page=add&action=add'>Add entries</a></li>" . LF;
+              print "<li class='left-level-1-no'><a href='" . $_SERVER["PHP_SELF"] . "?page=edit'>Edit entries</a></li>" .LF;
+              print "<div align='center'><li><b>" . $_SESSION["user_name"] . " logged in</b></li></div>" . LF;
+              print "<div align='center'><li><a href='" . $_SERVER["PHP_SELF"] . "?page=account'>My account</a></li></div>" . LF;
+              print "<div align='center'><li><input type='submit' class='cancel' name='logout' value='Log out'></li></div>" . LF;
               if ($_SESSION["user_level"] >= 31)
               {
-                print "<IMG src='images/blank.gif' height='30' border='0'>";
-                print "<div align='center'><li><A href='" . $_SERVER["PHP_SELF"] . "?page=registration'>Register a new account</A></li></div>" . LF;
+                print "<img src='images/blank.gif' height='30'>";
+                print "<div align='center'><li><A href='" . $_SERVER["PHP_SELF"] . "?page=registration'>Register a new account</a></li></div>" . LF;
               }
             }
           ?>
 
               </ul>
-              <IMG src="images/blank.gif" height="30" border="0">
-              <div align='center'><SPAN class="dhc"><B>Developed by&nbsp;&nbsp;</B><br /><A href="http://www.iwf.oeaw.ac.at/">IWF Graz Team</A></SPAN></div>
+              <img src="images/blank.gif" height="30"/>
+              <div align='center'><span class="dhc"><b>Developed by&nbsp;&nbsp;</b><br /><a href="http://www.iwf.oeaw.ac.at/">IWF Graz Team</a></span></div>
             </div>
             </div>
             </div>
             </div>
-          </TABLE>
-        </TD>
-        <TD valign="top"><IMG src="images/blank.gif" width="10" border="0"></TD>
-        <TD align="left" valign="top" width="770" class="rbody"><!-- LOAD MAIN FRAME -->
+          </table>
+        </td>
+        <td valign="top"><img src="images/blank.gif" width="10"/></td>
+        <td align="left" valign="top" width="770" class="rbody"><!-- LOAD MAIN FRAME -->
         
         <?php
         
@@ -190,16 +190,16 @@
             Controller::check("home");
         ?>
         
-        </TD>
-        <TD valign="top"><IMG src="images/blank.gif" width="10" border="0"></TD>
-      </TR>
-      <TR><TD valign="top" colspan="5"><IMG src="images/blank.gif" width="10" height="10" border="0"></TD></TR>
-    </TABLE>
-    </CENTER>
+        </td>
+        <td valign="top"><img src="images/blank.gif" width="10"/></td>
+      </tr>
+      <tr><td valign="top" colspan="5"><img src="images/blank.gif" width="10" height="10"/></td></tr>
+    </table>
+    </center>
       <div id="footer"><!-- FOOTER-->
         <div id="footer-box">
           <div id="ue">
-            <a href="http://europa.eu/index_en.htm" target="_blank"><img src="images/ue_logo_small.gif" width="40" height="23" border="0" alt="Project supported by the European Union." title="Project supported by the European Union." /></a>
+            <a href="http://europa.eu/index_en.htm" target="_blank"><img src="images/ue_logo_small.gif" width="40" height="23" alt="Project supported by the European Union." title="Project supported by the European Union." /></a>
             <a href="http://europa.eu/index_en.htm">Project supported <br />by the European Union</a>
           </div>
           <div id="footer-text">
