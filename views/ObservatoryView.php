9bdb230e73053c344b2492aafb4899f7cdb6be35
Florian Topf <florian.topf@oeaw.ac.at>
2011-04-28 17:15:34 +0000

    Trying to use STRICT here

--- views/ObservatoryView.php
+++ views/ObservatoryView.php
@@ -26,8 +26,8 @@ $link->close();
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html>
 <head>
