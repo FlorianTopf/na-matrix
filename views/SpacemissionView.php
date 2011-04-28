b4dfe09e44b87e8c52c2780cb9a94839bdab2039
Florian Topf <florian.topf@oeaw.ac.at>
2011-04-28 18:45:38 +0000

    made spacemission view STRICT compatible

--- views/SpacemissionView.php
+++ views/SpacemissionView.php
@@ -6,6 +6,7 @@
  *
  * @todo improve if clauses when we want to hide fields which are not given
  * @todo improve includes a bit
+ * @todo empty tables (without tr), empty lists (without li), a href targets are NOT allowed with xhtml strict
  */
 
 require_once ('../config.inc.php');
@@ -26,18 +27,18 @@ $link->close();
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
   <title>NA1-Matrix: <?php print $res["mission_name"]  ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
   <meta http-equiv="cache-control" content="no-cache, must-revalidate"/>
   <link rel="stylesheet" type="text/css" href="../css/style.css"/>
-<!--  <SCRIPT src="../lib/js/jquery-1.4.2.min.js"></SCRIPT>-->
-<!--  <SCRIPT src="../lib/js/jquery.validate.min.js"></SCRIPT>-->
-<!--  <SCRIPT src="../js/functions.js"></SCRIPT>-->
+<!--    <script type="text/javascript" src="lib/js/jquery-1.4.2.min.js"></script>-->
+<!--    <script type="text/javascript" src="lib/js/jquery.validate.min.js"></script>-->
+<!--    <script type="text/javascript" src="js/functions.js"></script>-->
 </head>
 
 <body class='report'>
@@ -51,65 +52,65 @@ $link->close();
     $_spacemission->get_resource($_GET["id"]);
 
     //SPACE MISSION GENERAL:
-  	print "<FIELDSET class='report'><LEGEND><B>Space Mission General:</B></LEGEND>" . LF;
-	print "<P><B>Space Mission name:&nbsp;</B>" .	$_spacemission->get_field("spa_mission_name") . "</P>" . LF;
+  	print "<fieldset class='report'><legend><b>Space Mission General:</b></legend>" . LF;
+	print "<p><b>Space Mission name:&nbsp;</b>" .	$_spacemission->get_field("spa_mission_name") . "</p>" . LF;
 	$agencies = $_spacemission->get_agencies();
-	print "<P><B>Agency:</B>&nbsp;<A href='" . $agencies['web_address'][$_spacemission->get_field("spa_mission_agency")] .
-		  "' target='_blank'>" . $agencies['acronym'][$_spacemission->get_field("spa_mission_agency")] . "</A></P>" . LF;
-	print "<P><B>Launch Date:&nbsp;</B>" . $_spacemission->get_field("spa_launch_date") . "&nbsp;[YYYY-MM-DD]</P>" . LF;
-	print "<P><B>Death Date:&nbsp;</B>" . $_spacemission->get_field("spa_death_date") . "&nbsp;[YYYY-MM-DD]</P>" . LF;
-	print "<P><B>Web address:&nbsp;</B><A href='" . $_spacemission->get_field("spa_web_address") . "' target='_blank'>" . $_spacemission->get_field("spa_web_address") . "</A></P>" . LF;
-	print "<P><B>Brief description:&nbsp;</B>" . nl2br($_spacemission->get_field("spa_brief_description")) . "</P>" . LF;
-	print "</FIELDSET>" . LF;
+	print "<p><b>Agency:</b>&nbsp;<a href='" . $agencies['web_address'][$_spacemission->get_field("spa_mission_agency")] .
+		  "' target='_blank'>" . $agencies['acronym'][$_spacemission->get_field("spa_mission_agency")] . "</a></p>" . LF;
+	print "<p><b>Launch Date:&nbsp;</b>" . $_spacemission->get_field("spa_launch_date") . "&nbsp;[YYYY-MM-DD]</p>" . LF;
+	print "<p><b>Death Date:&nbsp;</b>" . $_spacemission->get_field("spa_death_date") . "&nbsp;[YYYY-MM-DD]</p>" . LF;
+	print "<p><b>Web address:&nbsp;</b><a href='" . $_spacemission->get_field("spa_web_address") . "' target='_blank'>" . $_spacemission->get_field("spa_web_address") . "</a></p>" . LF;
+	print "<p><b>Brief description:&nbsp;</b>" . nl2br($_spacemission->get_field("spa_brief_description")) . "</p>" . LF;
+	print "</fieldset>" . LF;
 
 	//Research Areas:
-	print "<FIELDSET class='report'><LEGEND><B>Space Mission Research Areas:</B></LEGEND><UL>";
+	print "<fieldset class='report'><legend><b>Space Mission Research Areas:</b></legend><UL>";
 	$research_areas = $_spacemission->get_research_areas();
     foreach($research_areas['id'] as $key => $value)
     	if(is_array($_spacemission->get_has_many("research_areas", NULL)))
         	if (in_array($value, $_spacemission->get_has_many("research_areas", NULL)))
           		print "<li>" . $research_areas['name'][$key] . "</li>";
-	print "</UL></FIELDSET>" . LF;
+	print "</UL></fieldset>" . LF;
 
 	//Targets:
-	print "<FIELDSET class='report'><LEGEND><B>Space Mission Targets:</B></LEGEND> <UL>";
+	print "<fieldset class='report'><legend><b>Space Mission Targets:</b></legend> <UL>";
   	$targets = $_spacemission->get_targets();
     foreach($research_areas['id'] as $key => $value)
         if(is_array($_spacemission->get_has_many("targets", NULL)))
         	if (in_array($value, $_spacemission->get_has_many("targets", NULL)))
         	print "<li>" . $targets['target_family'][$key] . " - " . $targets['target_name'][$key] . "</li>" . LF;
-	print "</UL></FIELDSET>" . LF;
+	print "</UL></fieldset>" . LF;
 
 	//Sensors:
 	$science_goals = $_spacemission->get_science_goals();
 	if(is_array($_spacemission->get_has_many("sensors", NULL)))
 		foreach($_spacemission->get_has_many("sensors", NULL) as $sensor_count => $sensor_id)
 		{
-			print "<FIELDSET class='report'><LEGEND><B>Space Mission Sensor:&nbsp;" . $_spacemission->get_sensor("sensor_name", $sensor_count) . "</B></LEGEND>";
-			print "<P><B>Sensor Name:&nbsp;</B>" . $_spacemission->get_sensor("sensor_name", $sensor_count) . "</P>" . LF;
-			print "<P><B>Sensor Type:&nbsp;</B>" . $_spacemission->get_sensor("sensor_type", $sensor_count) . "</P>" . LF;
+			print "<fieldset class='report'><legend><b>Space Mission Sensor:&nbsp;" . $_spacemission->get_sensor("sensor_name", $sensor_count) . "</b></legend>";
+			print "<p><b>Sensor Name:&nbsp;</b>" . $_spacemission->get_sensor("sensor_name", $sensor_count) . "</p>" . LF;
+			print "<p><b>Sensor Type:&nbsp;</b>" . $_spacemission->get_sensor("sensor_type", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("underlying", $sensor_count))
-				print "<P><B>Underlying:&nbsp;</B>" . $_spacemission->get_sensor("underlying", $sensor_count) . "</P>" . LF;
+				print "<p><b>Underlying:&nbsp;</b>" . $_spacemission->get_sensor("underlying", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("range_begin", $sensor_count))
-				print "<P><B>Range Begin:&nbsp;</B>" . $_spacemission->get_sensor("range_begin", $sensor_count) . "</P>" . LF;
+				print "<p><b>Range Begin:&nbsp;</b>" . $_spacemission->get_sensor("range_begin", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("range_end", $sensor_count))
-				print "<P><B>Range End:&nbsp;</B>" . $_spacemission->get_sensor("range_end", $sensor_count) . "</P>" . LF;
+				print "<p><b>Range End:&nbsp;</b>" . $_spacemission->get_sensor("range_end", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("units", $sensor_count))
-				print "<P><B>Units:&nbsp;</B>" . $_spacemission->get_sensor("units", $sensor_count) . "</P>" . LF;
+				print "<p><b>Units:&nbsp;</b>" . $_spacemission->get_sensor("units", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("measured", $sensor_count))
-				print "<P><B>Measured:&nbsp;</B>" . $_spacemission->get_sensor("measured", $sensor_count) . "</P>" . LF;
+				print "<p><b>Measured:&nbsp;</b>" . $_spacemission->get_sensor("measured", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("resolution", $sensor_count))
-				print "<P><B>Resolution:&nbsp;</B>" . $_spacemission->get_sensor("resolution", $sensor_count) . "</P>" . LF;
+				print "<p><b>Resolution:&nbsp;</b>" . $_spacemission->get_sensor("resolution", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("field_of_view", $sensor_count))
-				print "<P><B>Field of View:&nbsp;</B>" . $_spacemission->get_sensor("field_of_view", $sensor_count) . "</P>" . LF;
+				print "<p><b>Field of View:&nbsp;</b>" . $_spacemission->get_sensor("field_of_view", $sensor_count) . "</p>" . LF;
 			if($_spacemission->get_sensor("web_address", $sensor_count))
-				print "<P><B>Web Address:&nbsp;</B><A href='" . $_spacemission->get_sensor("web_address", $sensor_count) .
-			 	"' target='_blank'>" . $_spacemission->get_sensor("web_address", $sensor_count) . "</A></P>" . LF;
+				print "<p><b>Web Address:&nbsp;</b><a href='" . $_spacemission->get_sensor("web_address", $sensor_count) .
+			 	"' target='_blank'>" . $_spacemission->get_sensor("web_address", $sensor_count) . "</a></p>" . LF;
 			if($_spacemission->get_sensor("sensor_comments", $sensor_count))
-				print "<P><B>Sensor Comments:&nbsp;</B>" . nl2br($_spacemission->get_sensor("sensor_comments", $sensor_count)) . "</P>" . LF;
+				print "<p><b>Sensor Comments:&nbsp;</b>" . nl2br($_spacemission->get_sensor("sensor_comments", $sensor_count)) . "</p>" . LF;
 
 			//Science Goals:
-//			print "<FIELDSET class='report'><LEGEND><B>Science Goals:</B></LEGEND> <UL>";
+//			print "<fieldset class='report'><legend><b>Science Goals:</b></legend> <ul>";
 //			//if($_spacemission->get_science_goals($_POST["add_spa_sci_goal_ids"][$sensor_count]))
 //			//	foreach ($_spacemission->get_science_goals($_POST["add_spa_sci_goal_ids"][$sensor_count]) as $key2 => $value2 )
 //			//	{
@@ -119,23 +120,23 @@ $link->close();
 //				if(is_array($_spacemission->get_has_many("science_goals", $sensor_id)))
 //					if(in_array($value, $_spacemission->get_has_many("science_goals", $sensor_id)))
 //						print "<li>" . $science_goals['name'][$key] . "</li>";
-//			print "</UL></FIELDSET>" . LF;
+//			print "</ul></fieldset>" . LF;
 
 			//Scientific Contacts:
-			print "<FIELDSET class='report'><LEGEND><B>Scientific Contacts:</B></LEGEND>";
-			print "<TABLE border='0' cellpadding='4' class='rtable' width='100%'>";
+			print "<fieldset class='report'><legend><b>Scientific Contacts:</b></legend>";
+			print "<table border='0' cellpadding='4' class='rtable' width='100%'>";
 			if(is_array($_spacemission->get_has_many("scientific_contacts", $sensor_id)))
 				foreach ($_spacemission->get_has_many("scientific_contacts", $sensor_id) as $contact_count => $contact_id)
 				{
 					print "<TR>";
 					print "<TD><b>Name:&nbsp;</b>" . $_spacemission->get_scientific_contact("name", $sensor_id, $contact_count) . "</TD>";
-					print "<TD><b>Email:&nbsp;</b><A href='mailto:" . $_spacemission->get_scientific_contact("email", $sensor_id, $contact_count) . "'>" .
+					print "<TD><b>Email:&nbsp;</b><a href='mailto:" . $_spacemission->get_scientific_contact("email", $sensor_id, $contact_count) . "'>" .
 						  $_spacemission->get_scientific_contact("email", $sensor_id, $contact_count) . "</TD>";
 					print "<TD><b>Institution:&nbsp;</b>" . $_spacemission->get_scientific_contact("institution", $sensor_id, $contact_count) . "</TD>";
 					print "</TR>";
 				}
-			print "</TABLE>";
-			print "</FIELDSET></FIELDSET>";
+			print "</table>";
+			print "</fieldset></fieldset>";
 		}
 		
 ?>
