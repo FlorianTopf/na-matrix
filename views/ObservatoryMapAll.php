05922b8f041a9ebc34df0ed772643fac28d5dba3
Florian Topf <florian.topf@oeaw.ac.at>
2011-09-12 16:27:09 +0000

    Modified Description Text of Map

--- views/ObservatoryMapAll.php
+++ views/ObservatoryMapAll.php
@@ -12,10 +12,10 @@ print "<div><input type='hidden' name='page' value='map'/></div>" . LF ;
 print "<h1>Interactive Map of ground-based Observatories</h1>";
 
 print "<p class='home'>
-On this map you can see the location of all currently integrated Observatories.
-If you agree to the browsers notification, it will determine your current location and 
-display it with a blue marker. The map is then centered to your location. You may get more 
-information on an observatory by clicking on one of the red markers. Note, that this service
+On this map you can see the location of all <b>currently integrated Observatories</b>.
+If you agree to the browsers notification, it will determine <b>your current location</b> and 
+display it with a <b>blue marker</b>. The map is then centered to your location. You may get <b>more 
+information</b> on an observatory by clicking on one of the <b>red markers</b>. Note, that this service
 is in experimental state and not fully functionable.
 </p>";
 
