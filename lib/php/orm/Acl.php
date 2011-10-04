95fa751b2c878d62f7c1a416d588ca2f95c21371
Robert Stoeckler <robert.stoeckler@oeaw.ac.at>
2011-10-04 09:25:54 +0000

    added ACL user_level description

--- lib/php/orm/Acl.php
+++ lib/php/orm/Acl.php
@@ -25,9 +25,13 @@ class Acl
 	);
 
 	static private $_profiles = array(
+	//Guest - can browse and see details on entries
 	'anonymous' => 0,
+	//Data Submitter - can submit OBS entries (approved = 0)
 	'browsing' => 11,
+	//Maintainer - can add, edit, approve (submitted data entries)
 	'processing' => 21,
+	//God - can add, edit, approve, delete entries and create Maintainer accounts
 	'superuser' => 31,
 	);
 
