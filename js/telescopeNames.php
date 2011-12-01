1dcdaba4245657eeb451a0ebcd5ee57eec832650
Florian Topf <florian.topf@oeaw.ac.at>
2011-12-01 15:25:07 +0000

    fixed rest of ajax files for utf8 problem!

--- js/telescopeNames.php
+++ js/telescopeNames.php
@@ -11,7 +11,7 @@
 
 // no term passed - just exit early with no response
 if (empty($_GET['term'])) exit ;
-$q = strtolower($_GET["term"]);
+$q = strtolower(utf8_decode($_GET["term"]));
 // remove slashes if they were magically added
 if (get_magic_quotes_gpc()) $q = stripslashes($q);
 
@@ -27,8 +27,9 @@ if ($link->getNumRows($result) > 0)
 	$return = array();
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 	{
-		array_push($return, array("id"=>$row['name'], "label"=>$row['name'],
-			 "value" => strip_tags($row['name'])));
+		array_push($return, array("id" => utf8_encode($row['name']), 
+			"label" => utf8_encode($row['name']), 
+			"value" => strip_tags(utf8_encode($row['name']))));
 	}
 }
 else
--- js/wavelengthRanges.php
+++ js/wavelengthRanges.php
@@ -11,7 +11,7 @@
 
 // no term passed - just exit early with no response
 if (empty($_GET['term'])) exit ;
-$q = strtolower($_GET["term"]);
+$q = strtolower(utf8_decode($_GET["term"]));
 // remove slashes if they were magically added
 if (get_magic_quotes_gpc()) $q = stripslashes($q);
 
@@ -27,8 +27,9 @@ if ($link->getNumRows($result) > 0)
 	$return = array();
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 	{
-		array_push($return, array("id"=>$row['acronym'], "label"=>$row['acronym'] .
-		" [" . $row['name'] . "]" , "value" => strip_tags($row['acronym'])));
+		array_push($return, array("id" => utf8_encode($row['acronym']), 
+			"label" => utf8_encode($row['acronym']) . " [" . utf8_encode($row['name']) . "]" , 
+			"value" => strip_tags(utf8_encode($row['acronym']))));
 	}
 }
 else
